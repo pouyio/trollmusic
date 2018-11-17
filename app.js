c820518744e4f75b98060c2aa938fa89ca3bb940
Vicente <pouyio@gmail.com>
2018-11-17 17:33:57 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const http = require('http');
+const bodyParser = require('body-parser')
+const app = express();
+const server = http.createServer(app);
+const io = require('socket.io').listen(server);
+const PORT = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+io.on('connection', (socket) => {
+    socket.on('set-user', (user, fn) => {
+        socket.data = { user };
+        fn(true);
+    })
+});
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+
+app.get('/videos', (req, res) => {
+    res.json({ videos: true });
+});
+
+app.post('/video', (req, res) => {
+    io.emit('video-added', req.body.user, req.body.video);
+    res.end();
+});
+
+app.get('/users', (req, res) => {
+    const users = [];
+    Object.keys(io.sockets.connected).forEach((socketID) => {
+        users.push(io.sockets.connected[socketID].data);
+    });
+    res.send(users)
+});
+
+
+server.listen(PORT, () => console.log(`Started on port ${PORT}!`));
