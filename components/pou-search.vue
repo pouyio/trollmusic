c19a1d48faafbebef3cf45f9837ec0e8b014fdee
Vicente <pouyio@gmail.com>
2018-12-26 14:39:25 +0100

    improved search visual

--- components/pou-search.vue
+++ components/pou-search.vue
@@ -3,7 +3,7 @@
     <div>
       <input
         v-model="searchText"
-        class="transition outline-none border border-transparent bg-orange-lighter focus:border-orange-light rounded focus:bg-white focus:shadow-md py-2 pr-4 pl-10 block w-full appearance-none leading-normal"
+        class="transition outline-none border border-transparent bg-orange-lighter focus:border-orange-light rounded focus:bg-white focus:shadow-md py-2 pr-4 pl-4 md:pl-10 block w-full appearance-none leading-normal"
         type="text"
         placeholder="🔍 Search video..."
       >
