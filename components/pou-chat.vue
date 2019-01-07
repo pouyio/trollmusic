09cb1b98e47f4d5fbd4ff6aaeaf7da7a610b21ba
vortizga <vortizga@everis.com>
2019-01-07 17:04:23 +0100

    true fix

--- components/pou-chat.vue
+++ components/pou-chat.vue
@@ -83,6 +83,7 @@ export default {
   },
   methods: {
     submit() {
+      this.message = this.message.trim();
       if (!this.message) {
         return;
       }
@@ -96,7 +97,7 @@ export default {
   sockets: {
     message({ user, message }) {
       this.messages.push([user, message]);
-      if (this.user !== user) {
+      if (this.user === user) {
         this.$notification.show(user, {
           body: message,
           icon: 'https://f4.bcbits.com/img/0010573837_20.jpg'
