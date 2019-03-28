e51245e30051ed636959037a9b894130603b4f4c
Vicente <pouyio@gmail.com>
2019-03-28 20:21:29 +0100

    fix conected users

--- ably/ably.js
+++ ably/ably.js
@@ -2,10 +2,13 @@ import * as Ably from 'ably';
 
 let channel;
 let clientId = "";
-const setCliendId = (id) => clientId = id;
 
-const client = new Ably.Realtime({ key: '9Dakxw.tSDnZA:kRUK23uiauySw7gM', clientId });
-channel = client.channels.get('trollmusic');
-channel.presence.enter();
+const setCliendId = (id) => {
+    clientId = id;
+    const client = new Ably.Realtime({ key: '9Dakxw.tSDnZA:kRUK23uiauySw7gM', clientId });
+    channel = client.channels.get('trollmusic');
+    channel.presence.enter();
+};
+
 
 export { channel, clientId as user, setCliendId }; 
