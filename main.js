fad99f33b81f94b4524e0f0d50284096795c2641
Vicente <pouyio@gmail.com>
2019-04-02 19:43:38 +0200

    added sanitize messages to chat

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import VueYouTubeEmbed from 'vue-youtube-embed';
 import VueChatScroll from 'vue-chat-scroll';
 import VueNativeNotification from 'vue-native-notification';
 import VueFirestore from 'vue-firestore';
+import VueSanitize from 'vue-sanitize';
 import './style.css'
 
 import App from './App.vue';
@@ -11,6 +12,12 @@ Vue.use(VueFirestore);
 Vue.use(VueYouTubeEmbed);
 Vue.use(VueNativeNotification, { requestOnNotify: true });
 Vue.use(VueChatScroll);
+Vue.use(VueSanitize, {
+    allowedTags: ['a'],
+    allowedAttributes: {
+        'a': ['href', 'target']
+    }
+});
 
 new Vue({
     el: "#app",
